232b41fe9d4b8f64341bf8f1071edfb834a1a29c
Scott Taylor <scott@railsnewbie.com>
2009-03-07 04:14:33 -0500

    Move Sinatra top level into sinatra.rb

--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -1,7 +1,38 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require 'sinatra/base'
+require 'thread'
+require 'time'
+require 'uri'
+require 'rack'
+require 'rack/builder'
+
+module Sinatra
+  VERSION = '0.9.1'
+
+  class NotFound < NameError #:nodoc:
+    def code ; 404 ; end
+  end
+  
+  dir = File.dirname(__FILE__) + "/sinatra"
+  require "#{dir}/request"
+  require "#{dir}/response"
+  require "#{dir}/helpers"
+  require "#{dir}/templates"
+  require "#{dir}/real_base"
+  require "#{dir}/default_base"
+  require "#{dir}/core_extensions"
+  require "#{dir}/delegator"
+  require "#{dir}/top_level_helpers"
+
+  extend TopLevelHelpers
+
+  # The top-level Application. All DSL methods executed on main are delegated
+  # to this class.
+  class Application < Default
+  end
+end
+
 require 'sinatra/main'
 require 'sinatra/compat'
 
