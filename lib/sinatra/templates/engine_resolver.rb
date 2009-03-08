aa1ee4f14093badff0e526cf4ec1e7dd8aa8cf12
Scott Taylor <scott@railsnewbie.com>
2009-03-08 16:01:59 -0400

    Don't create an unnecessary object for each render request

--- lib/sinatra/templates/engine_resolver.rb
+++ lib/sinatra/templates/engine_resolver.rb
@@ -1,24 +1,22 @@
 module Sinatra
   module Templates
     class EngineResolver
-      def self.resolve(engine, context)
-        new.resolve(engine, context)
-      end
+      class << self
+        class EngineNotFound < StandardError; end
 
-      class EngineNotFound < StandardError; end
-
-      def resolve(engine, context)
-        case engine
-        when :erb
-          RenderingEngine::ERBRenderer.new(context)
-        when :builder
-          RenderingEngine::BuilderRenderer.new(context)
-        when :haml
-          RenderingEngine::HamlRenderer.new(context)
-        when :sass
-          RenderingEngine::SassRenderer.new(context)
-        else
-          raise EngineNotFound, "Could not find an engine for '#{engine}'"
+        def resolve(engine, context)
+          case engine
+          when :erb
+            RenderingEngine::ERBRenderer.new(context)
+          when :builder
+            RenderingEngine::BuilderRenderer.new(context)
+          when :haml
+            RenderingEngine::HamlRenderer.new(context)
+          when :sass
+            RenderingEngine::SassRenderer.new(context)
+          else
+            raise EngineNotFound, "Could not find an engine for '#{engine}'"
+          end
         end
       end
     end
