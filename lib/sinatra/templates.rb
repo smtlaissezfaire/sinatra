0f6a74f1bd7e386e8711f3539713a5c75bb078cf
Scott Taylor <scott@railsnewbie.com>
2009-03-08 00:30:32 -0500

    Use more accurate (and shorter) require statements

--- lib/sinatra/templates.rb
+++ lib/sinatra/templates.rb
@@ -2,8 +2,10 @@ module Sinatra
   # Template rendering methods. Each method takes a the name of a template
   # to render as a Symbol and returns a String with the rendered output.
   module Templates
-    require "sinatra/templates/helpers"
-    require "sinatra/templates/engine_resolver"
-    require "sinatra/templates/template_resolver"
+    dir = File.dirname(__FILE__) + "/templates"
+
+    require "#{dir}/helpers"
+    require "#{dir}/engine_resolver"
+    require "#{dir}/template_resolver"
   end
 end
